c85f790cf0bfb22afbec42bc399b92c17ad7bb15
Jude Kallista-Fitzpatrick <31487832+jsk2319@users.noreply.github.com>
2018-04-24 16:43:35 -0400

    Update FilmDemo.java

--- FilmDemo.java
+++ FilmDemo.java
@@ -2,9 +2,32 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class FilmDemo {
+    public static Film[] filmArray = new Film[10];
+
+    public static void addToList(Film newFilm){
+        boolean inArray = false;
+        for(int i = 0; i < filmArray.length; i++){
+            if(filmArray[i] == null){
+
+            }
+            else if(newFilm.equals(filmArray[i])){
+                inArray = true;
+            }
+        }
+        if(!inArray){
+            for(int i = 0; i < filmArray.length; i++) {
+                if(filmArray[i] == null) {
+                    filmArray[i] = newFilm;
+                    i = filmArray.length;
+                }
+            }
+        }
+    }
     public static void main(String[] args) {
         Scanner keyboard = new Scanner(System.in);
-        Film[] filmArray = new Film[10];
+        int tempYear;
+        String tempDirector;
+        String tempName;
 
         int Option;
 
@@ -18,12 +41,19 @@ public class FilmDemo {
             Option = keyboard.nextInt();
             keyboard.nextLine();
 
-             if (Option == 1) {
-                System.out.println("Please type the name and the director of the movie you want to add.");
+            if (Option == 1) {
+                System.out.println("What year is the release of the movie?");
+                tempYear = keyboard.nextInt();
+                System.out.println("What is the name of the director?");
+                tempDirector = keyboard.next();
+                System.out.println("What is the name of the movie?");
+                tempName = keyboard.next();
+                addToList(new Film(tempName,tempDirector,tempYear));
 
             }if (Option == 2) {
                 System.out.println("Here are the contents of the array:");
-                System.out.println(Arrays.toString(filmArray));
+                displayFilms(filmArray);
+
             }if (Option == 3) {
 
             }
@@ -31,5 +61,17 @@ public class FilmDemo {
         } while (Option !=3);
 
 
+    }
+    public static void displayFilms(Film[] list){
+        for(int i = 0; i < list.length;i++){
+            if(filmArray[i] == null){
+
+            }
+            else{
+                System.out.print("Name: " + list[i].getFilmName());
+                System.out.print(" Director: " + list[i].getDirectorName());
+                System.out.println(" Year Produced: " + list[i].getFilmYear());
+            }
+        }
     }
 }
